8f53f5728cfac2e00aea85b6a62b973794161a54
Marcus Pointner <marcus.pointner@gmail.com>
2014-08-06 23:09:31 +0300

    no adding to Doku-class in static main method
    in Haxepunk the Main class (which extends com.haxepunk.Engine) does not
    need to be added separately to the Dokument class. This is done
    automatically... (appears like that)

--- src/MainStablexHXP.hx
+++ src/MainStablexHXP.hx
@@ -27,6 +27,38 @@ class MainStablexHXP extends Engine
 		HXP.console.enable();
 #end
 		HXP.scene = new MainScene();
+	}
+	
+	public function constructStablexUI() {
+		
+		// ******** ALL KIND OF CUSTOM INITIALIZATION STUFF ********** 
+		// ( stablexui, etc..)
+
+		//register main class to access it's methods and properties in xml
+		//UIBuilder.regClass('Main');
+		
+        //Register our custom widget, so we can use it in xml
+        UIBuilder.regClass('ColorWidget');		
+		trace("regClass('ColorWidget'...");		
+	
+		//initialize StablexUI
+		//UIBuilder.init('ui/android/defaults.xml');
+		UIBuilder.init();
+		trace("initializing Stablex UI Builder ...");
+		
+		//register skins
+		//UIBuilder.regSkins('ui/android/skins.xml');
+	
+		//create callback for alert popup
+		//Main.alert = UIBuilder.buildFn('ui/alert.xml');
+	
+		//Create our UI
+		var ui = UIBuilder.buildFn('ui/index.xml')();
+		//ui.show();
+		
+		//Lib.current.addChild(ui);
+		Lib.current.stage.addChild(ui);
+		//addChild(ui);
 	}	
 	
     /**
@@ -35,15 +67,11 @@ class MainStablexHXP extends Engine
     */
     static public function main () : Void {
 		
-        UIBuilder.regClass('ColorWidget');
-        UIBuilder.init();
-		
 		Lib.current.stage.align = StageAlign.TOP_LEFT;
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		Lib.current.addChild(new MainStablexHXP());		
-
-        //Create our UI
-        //Lib.current.addChild( UIBuilder.buildFn('main.xml')() );
+		//Lib.current.addChild(new MainStablexHXP());
+		new MainStablexHXP();
+	
 		
     }//end function main()	
 	
