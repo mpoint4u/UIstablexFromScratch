94989b59c808e9deb26bc4343616353016098853
Marcus Pointner <marcus.pointner@gmail.com>
2014-08-08 12:30:39 +0300

    cleaned up a little bit
    just to make stuff more readable, etc

--- src/MainStablexHXP.hx
+++ src/MainStablexHXP.hx
@@ -6,7 +6,6 @@ import openfl.display.StageAlign;
 import openfl.display.StageScaleMode;
 import openfl.Lib;
 import ru.stablex.ui.UIBuilder;
-//import com.haxepunk.RenderMode;
 
 /**
  * ...
@@ -15,40 +14,35 @@ import ru.stablex.ui.UIBuilder;
 class MainStablexHXP extends Engine
 {
 
-/*	public function new(width:Int=0, height:Int=0, frameRate:Float=60, fixed:Bool=false, ?renderMode:RenderMode) 
-	{
-		super(width, height, frameRate, fixed, ?renderMode);
-		
-	}*/
+/* // ... implicit constructor used right NOW, but could be overridden like this...
+ * public function new(width:Int=0, height:Int=0, frameRate:Float=60, fixed:Bool=false, ?renderMode:RenderMode) {	
+ * 		super(width, height, frameRate, fixed, ?renderMode);
+ * 	} 
+ * */
 
-	override public function init()
-	{
+	override public function init(){  
 		// ******** ALL KIND OF CUSTOM INITIALIZATION STUFF ********** 
 		
 		// Stablexui init first !! ... 
-
-		//register main class to access it's methods and properties in xml
-		//UIBuilder.regClass('Main');
+			//register main class to access it's methods and properties in xml
+			//UIBuilder.regClass('Main');
 		
         //Register our custom widget, so we can use it in xml
         UIBuilder.regClass('ColorWidget');		
-		trace("regClass('ColorWidget'...");		
-	
-		//initialize StablexUI
+												//trace("regClass('ColorWidget'...");			
 		//UIBuilder.init('ui/android/defaults.xml');
 		UIBuilder.init();
-		trace("initializing Stablex UI Builder ...");
-		
+												//trace("initializing Stablex UI Builder ...");	
 		//register skins
-		//UIBuilder.regSkins('ui/android/skins.xml');		
+		//UIBuilder.regSkins('ui/android/skins.xml');
 		
-		
+		// HaxePunk is next !! ... 			
 #if debug
 		HXP.console.enable();
-		trace("HXP.console.enabled ...");
+												//trace("HXP.console.enabled ...");
 #end
 		HXP.scene = new MainScene();
-		trace("HXP.scene initialized ...");
+												//trace("HXP.scene initialized ...");
 		
 		constructStablexUI();
 	}
@@ -68,19 +62,17 @@ class MainStablexHXP extends Engine
 		//addChild(ui);
 	}	
 	
-    /**
-    * Enrty point
-    *
-    */
+    /** Entry point **/
     static public function main () : Void {
 		
 		Lib.current.stage.align = StageAlign.TOP_LEFT;
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		//Lib.current.addChild(new MainStablexHXP());
-		new MainStablexHXP();
-	
-		
-    }//end function main()	
-	
+
+		new MainStablexHXP();   /*
+								normally like   
+									Lib.current.addChild(new MainStablexHXP());
+								but constructor of Engine adds it automatically !!
+								*/		
+    }//end function main()	 
 	
 }// end of class MainStablexHXP
