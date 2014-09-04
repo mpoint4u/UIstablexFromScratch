e762117e67bee26810fa8ca15a1334f12479bf69
Marcus Pointner <marcus.pointner@gmail.com>
2014-09-04 07:20:52 +0200

    make example more interesting

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -12,7 +12,7 @@ class MainScene extends Scene
 {
 	// defining class members	
 	private var block:Entity;
-
+	private var ent2:Entity;
 	private var mywidget:HBox;
 	//private var mywidget:Widget;
 	
@@ -27,7 +27,8 @@ class MainScene extends Scene
 															trace(" overridden function begin() was called ... ");	
 		
 		// add the png-image to the Entity
-		block = addGraphic(new Image("assets/img/nme.png"));
+		block = addGraphic(new Image("assets/img/block.png"));		
+		ent2  = addGraphic(new Image("assets/img/nme.png"));
 		
 		// create the widget
 		mywidget = UIBuilder.buildFn('ui/index.xml')();
@@ -35,7 +36,7 @@ class MainScene extends Scene
 		//Lib.current.addChild(mywidget);
 		mywidget.left = 10;
 		mywidget.top = 10;
-		Lib.current.stage.addChildAt(mywidget, 0);
+		Lib.current.stage.addChildAt(mywidget, 1);			// 1 puts Widget over the entities
 	}
 
 	public override function update() {
@@ -48,8 +49,16 @@ class MainScene extends Scene
 			block.x = 0;
 			block.y += 50;
 		}
+
+		if( ent2.y < Lib.current.stage.stageHeight ){
+		      ent2.moveBy(0, 2);
+		} else {
+			ent2.y = 0;
+			ent2.x += 30; 
+		}
 		
-/*
+/* ***** Widget movement *********
+		// horizontal			
 		if ( mywidget.x < Lib.current.stage.stageWidth) 
 		{
 			mywidget.x += .5;			
@@ -57,13 +66,14 @@ class MainScene extends Scene
 			mywidget.x = 0;
 		}
 		
-		*/
+		// vertical
 		if ( mywidget.y < Lib.current.stage.stageHeight) 
 		{
 			mywidget.y += 3;			
 		} else {
 			mywidget.y = 0;
 		}
+		*********************** */
 		
 		super.update();
 	}		
