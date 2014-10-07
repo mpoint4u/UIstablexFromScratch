a1a10146bf3acf6dd540f55d2bb64025c865a8a4
Marcus Pointner <marcus.pointner@gmail.com>
2014-10-07 09:35:53 +0300

    cleaning up MainScene

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -10,7 +10,7 @@ import ru.stablex.ui.widgets.Widget;
 class MainScene extends Scene
 {
 	// defining class members	
-	private var block:Entity;
+	private var ent1:Entity;
 	private var ent2:Entity;
 	//private var mywidget:HBox;
 	private var mywidget:Widget;
@@ -27,66 +27,49 @@ class MainScene extends Scene
 															trace(" overridden function begin() was called ... ");	
 		
 		// add the png-image to the Entity
-		block = addGraphic(new Image("assets/img/block.png"));		
+		ent1 = addGraphic(new Image("assets/img/block.png"));		
 		ent2  = addGraphic(new Image("assets/img/nme.png"));
-		
-		
-		// create the color widget
-		//colorWid = new ColorWidget();
-/*		colorWid.id = "custom";
-		colorWid.w  = 200;
-		colorWid.h  = 100;
-		colorWid.color = 0x8080C0;*/
-		
-		// create the main widget
-		mywidget = UIBuilder.buildFn('ui/index.xml')();
-															// trace(Type.typeof(mywidget));	
-		//Lib.current.addChild(mywidget);
-		mywidget.left = 10;
-		mywidget.top = 10;
-		Lib.current.stage.addChildAt(mywidget, 1);			// 1 puts Widget over the entities
-		
-		//Lib.current.stage.addChildAt(colorWid, 2);			// 1 puts Widget over the entities
-		
-		
+			
+		createMainWidget();
 	}
 
+	
 	public override function update() {
 		//trace(" overridden function update() was called ... ");
 
-		if ( block.x < Lib.current.stage.stageWidth ) 
+		controlEnt1();
+		controlEnt2();
+		
+		super.update();
+	}		
+	
+	function createMainWidget():Void 
+	{
+		// create the main widget
+		mywidget = UIBuilder.buildFn('ui/index.xml')();	
+		mywidget.left = 10;
+		mywidget.top = 10;
+		Lib.current.stage.addChildAt(mywidget, 1);			// 1 puts Widget over the entities	
+	}
+	
+	function controlEnt1():Void 
+	{
+		if ( ent1.x < Lib.current.stage.stageWidth ) 
 		{
-			block.moveBy(3, 0);			
+			ent1.moveBy(3, 0);			
 		} else {			
-			block.x = 0;
-			block.y += 50;
+			ent1.x = 0;
+			ent1.y += 50;
 		}
-
+	}
+	
+	function controlEnt2():Void 
+	{
 		if( ent2.y < Lib.current.stage.stageHeight ){
-		      ent2.moveBy(0, 2);
+			  ent2.moveBy(0, 2);
 		} else {
 			ent2.y = 0;
 			ent2.x += 30; 
 		}
-		
-/* ***** Widget movement *********
-		// horizontal			
-		if ( mywidget.x < Lib.current.stage.stageWidth) 
-		{
-			mywidget.x += .5;			
-		} else {
-			mywidget.x = 0;
-		}
-		
-		// vertical
-		if ( mywidget.y < Lib.current.stage.stageHeight) 
-		{
-			mywidget.y += 3;			
-		} else {
-			mywidget.y = 0;
-		}
-		*********************** */
-		
-		super.update();
-	}		
+	}
 }
