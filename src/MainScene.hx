6c158f861b5d1e22cce393726d2c92cc99e92a22
Marcus Pointner <marcus.pointner@gmail.com>
2014-10-07 09:51:08 +0300

    more cleaning of MainScene

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -12,30 +12,27 @@ class MainScene extends Scene
 	// defining class members	
 	private var ent1:Entity;
 	private var ent2:Entity;
-	//private var mywidget:HBox;
 	private var mywidget:Widget;
-	//private var colorWid:ColorWidget;
 	
 	// constructor
 	public function new() {
 		super();
-		//constructStablexUI();
 	}
 
 	// methods
 	public override function begin() {
-															trace(" overridden function begin() was called ... ");	
+															trace("overridden function begin() was called ... ");	
 		
-		// add the png-image to the Entity
+		// add the png-images to the Entities
 		ent1 = addGraphic(new Image("assets/img/block.png"));		
 		ent2  = addGraphic(new Image("assets/img/nme.png"));
 			
-		createMainWidget();
+		_createMainWidget();
 	}
 
 	
 	public override function update() {
-		//trace(" overridden function update() was called ... ");
+		//trace("function update() was called ... ");
 
 		controlEnt1();
 		controlEnt2();
@@ -43,7 +40,7 @@ class MainScene extends Scene
 		super.update();
 	}		
 	
-	function createMainWidget():Void 
+	private function _createMainWidget():Void 
 	{
 		// create the main widget
 		mywidget = UIBuilder.buildFn('ui/index.xml')();	
